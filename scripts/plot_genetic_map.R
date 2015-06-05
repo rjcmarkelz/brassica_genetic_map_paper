936fc4d7e88a7377a0504215412f08a32bd1a3dc
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-06-05 08:22:36 -0700

    make faceted genetic map

--- scripts/plot_genetic_map.R
+++ scripts/plot_genetic_map.R
@@ -12,9 +12,20 @@ out <- as.data.frame(pull.geno(genetic_map))
 str(out)
 
 out <- scanone(genetic_map, pheno.col = 2)
-out2 <- head(out, 20)
-out2$len <- 3
+out2 <- out
+head(out2)
+out2$len <- 1
+out2$xout   <- 1
+out2$pos
+str(out2)
 
-b <- ggplot(out2, aes(pos, len)) + geom_point()
-b + geom_segment(aes(x = 2, y = 2, xend = 2, yend = 2))
-geom_pointrange(limits)
+
+b <- ggplot(out2, aes(len, pos)) + geom_point() + facet_grid(. ~ chr)
+b + geom_errorbarh(aes(xmax = xout + 5, xmin = xout - 5, height = 0))
+
+
+
+
+
+
+
