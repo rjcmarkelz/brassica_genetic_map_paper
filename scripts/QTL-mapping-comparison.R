74ff78949c472a7ae772103b643d6a6877135ae3
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-06-07 10:40:43 -0700

    finish QTL comparison plots

--- scripts/QTL-mapping-comparison.R
+++ scripts/QTL-mapping-comparison.R
@@ -1,10 +1,12 @@
-library(qtl)
+
 ###########
 # Old Map
 ###########
+library(qtl)
+library(ggplot2)
 setwd("/Users/Cody_2/git.repos/brassica_genetic_map_paper/input")
-brassica_traits <- read.cross("csvsr", genfile ="old_map_rqtl_missing_RILS_removed.csv", 
-	                       phefile="Brock_2010_phenotype.csv", genotypes=c("0","2"))
+brassica_traits <- read.cross("csvsr", genfile ="old_map_rqtl.csv", 
+	                       phefile="Brock_2010_pheno.csv", genotypes=c("0","2"))
 head(brassica_traits)
 class(brassica_traits)[1] <- "riself"
 brassica_traits <- jittermap(brassica_traits)
@@ -21,7 +23,14 @@ plot(brassica_traits)
 brassica_traits
 plot.map(brassica_traits)
 
-old <- scanone(brassica_traits, pheno.col = 2)
+#change the names of the chromosomes
+old_map <- pull.map(brassica_traits)
+str(brassica_traits)
+names(brassica_traits$geno) <- paste(c("A01", "A02", "A03", "A04", "A05", "A06", "A07", "A08", "A09", "A10"))
+names(brassica_traits$geno)
+
+old <- scanone(brassica_traits, pheno.col = 1, method = "imp")
+old
 old2 <- old
 head(old2)
 old2$len <- 1
@@ -32,7 +41,7 @@ old_map_plot <- ggplot(old2, aes(len, pos, ymin = 0, ymax = 40)) + facet_grid(. 
 old_map_plot <- old_map_plot + geom_segment(aes(x = len - 5, y = pos, xend = len + 5, yend = pos))
 old_map_plot <- old_map_plot + geom_segment(aes(x = len, y = 0, xend = len, yend = pos))
 old_map_plot <- old_map_plot + scale_y_reverse()
-old_map_plot <- old_map_plot + theme(panel.background = NULL, axis.text.x = element_blank(),
+old_map_plot <- old_map_plot + theme(panel.background = element_blank(), axis.text.x = element_blank(),
                              axis.line = element_blank(), panel.grid.major = element_blank(), 
                              panel.grid.minor = element_blank(), axis.ticks.x = element_blank(), 
                              axis.title.x = element_blank())
@@ -43,62 +52,31 @@ setwd("/Users/Cody_2/git.repos/brassica_genetic_map/output")
 ggsave("old_genetic_map.pdf", old_map_plot)
 
 
+oldleaf <- scanone(brassica_traits, pheno.col = 1, method = "imp", chr = "A06")
+plot(oldleaf)
 
-peak <- max(scanone_2011_flr$lod)
-oldmapplotflr <- ggplot(scanone_2011_flr)
-oldmapplotflr <- oldmapplotflr +  theme_bw() + geom_line(aes(x = pos, y = lod), size = 2) +
+peak <- 7
+oldmapplotleaf <- ggplot(oldleaf)
+oldmapplotleaf <- oldmapplotleaf +  theme_bw() + scale_y_continuous(limits=c(0, 7.5)) + 
+                        geom_line(aes(x = pos, y = lod), size = 2) +
                         geom_hline(yintercept = 2.44, color = "red", size = 1) +
                         geom_segment(aes(x = pos, xend = pos), y = (peak * -0.02), yend = (peak * -0.05)) +
-                        theme(text = element_text(size = 20))
+                        theme(text = element_text(size = 20)) +
                         # theme(legend.position = "none",
                         #   axis.text.x = element_text(angle = 90),
                         #   axis.line=element_line())
                         #   # panel.margin = unit(0, "cm")) +
                         # ggtitle("LOD Curves for QTLs") +
-                        #xlab("Position in cM") +
-                        #ylab("LOD Score") 
-oldmapplotflr
+                        xlab("Genetic Position (cM)") +
+                        ylab("LOD Score") 
+oldmapplotleaf
 
-###########
-# New Map
-###########
-brassica_newmap <- read.cross("csvsr", genfile ="Brassica_F8_v1.0_gen.csv", 
-	                       phefile="Brock_2010_phenotype.csv", genotypes=c("AA","BB"))
-head(brassica_newmap)
-class(brassica_newmap)[1] <- "riself"
-brassica_newmap <- jittermap(brassica_newmap)
-brassica_newmap
-
-brassica_newmap <- est.rf(brassica_newmap)
-plot.rf(brassica_newmap) 
-brassica_newmap
-newmap <- est.map(brassica_newmap,verbose=T,error.prob=.01)
-
-pairs(jitter( as.matrix(brassica_newmap$pheno) ), cex=0.6, las=1)
-
-plot.map(brassica_newmap,newmap) #some compression in this brassica_newmap set
-brassica_newmap
-
-br_phys <- read.cross("csvsr", genfile ="Brassica_F8_v2.1_gen.csv", 
-                         phefile="br_blups_RQTL.csv", 
-                         genotypes=c("AA","BB"), na.strings = c("-","NA"))
-class(br_phys)[1] <- "riself"
-br_phys
-so.perm2 <- scanone(br_phys, method = "imp", n.perm = 1000) 
-summary(so.perm2)
-so.perm295 <- summary(so.perm2)[1] #keep 95%
-# LOD thresholds (1000 permutations)
-#      lod
-# 5%  3.23
-# 10% 2.81
-
-?scanone
-scanone_2011_STP <- scanone(br_phys, pheno.col = "X2011_STP", method = "imp", use="all.obs", chr = "A03")
-plot(scanone_2011_STP, chr = "A03")
-
-peak2 <- max(scanone_2011_STP$lod)
-newplot_map <- ggplot(scanone_2011_STP)
-newplot_map <- newplot_map +  theme_bw() + geom_line(aes(x = pos, y = lod), size = 2) +
+oldmapplotflr <- scanone(brassica_traits, pheno.col = 2, method = "imp", chr = "A10")
+plot(oldmapplotflr)
+peak2 <- 9
+oldplot_flr <- ggplot(oldmapplotflr)
+oldplot_flr <- oldplot_flr +  theme_bw() + scale_y_continuous(limits=c(0, 13)) + 
+                        geom_line(aes(x = pos, y = lod), size = 2) +
                         geom_hline(yintercept = 3.08, color = "red", size = 1) +
                         geom_segment(aes(x = pos, xend = pos), y = (peak * -0.02), yend = (peak * -0.05)) +
                         theme(text = element_text(size = 20)) +
@@ -107,21 +85,76 @@ newplot_map <- newplot_map +  theme_bw() + geom_line(aes(x = pos, y = lod), size
                         #   axis.line=element_line())
                         #   # panel.margin = unit(0, "cm")) +
                         # ggtitle("LOD Curves for QTLs") +
-                        xlab("Genetic Distance Along Chromosome") +
+                        xlab("Genetic Distance (cM)") +
+                        ylab("LOD Score") 
+oldplot_flr
+
+
+###########
+# New Map
+###########
+setwd("/Users/Cody_2/git.repos/brassica_genetic_map/output")
+brassica_newmap <- read.cross("csvsr", genfile ="snp_map_rqtl_Mbp_ref1.5_cross_output_gen.csv", 
+                           phefile="snp_map_rqtl_Mbp_ref1.5_cross_output_phe.csv", 
+                           genotypes=c("AA","BB"), 
+                           na.strings=c("NA","-"))
+head(brassica_newmap)
+class(brassica_newmap)[1] <- "riself"
+brassica_newmap <- jittermap(brassica_newmap)
+brassica_newmap
+
+newmapplotleaf_perm <- scanone(brassica_newmap, pheno.col = 1, method = "imp") 
+summary(so.perm2)
+# so.perm295 <- summary(so.perm2)[1] #keep 95%
+# # LOD thresholds (1000 permutations)
+# #      lod
+# # 5%  3.23
+# # 10% 2.81
+
+newmapplotleaf <- scanone(brassica_newmap, pheno.col = 1, method = "imp", chr = "A06")
+plot(newmapplotleaf)
+
+peak2 <- 7
+newplot_map <- ggplot(newmapplotleaf)
+newplot_map <- newplot_map +  theme_bw() + scale_y_continuous(limits=c(0, 7.5)) + 
+                        geom_line(aes(x = pos, y = lod), size = 2) +
+                        geom_hline(yintercept = 3.08, color = "red", size = 1) +
+                        geom_segment(aes(x = pos, xend = pos), y = (peak * -0.02), yend = (peak * -0.05)) +
+                        theme(text = element_text(size = 20)) +
+                        # theme(legend.position = "none",
+                        #   axis.text.x = element_text(angle = 90),
+                        #   axis.line=element_line())
+                        #   # panel.margin = unit(0, "cm")) +
+                        # ggtitle("LOD Curves for QTLs") +
+                        xlab("Genetic Distance (cM)") +
                         ylab("LOD Score") 
 newplot_map
 
 
-brassica_newmap <- replace.map(brassica_newmap,newmap) #use new map
-plot(brassica_newmap) 
-brassica_newmap
-plot.map(brassica_newmap)
-brassica_newmap <- shiftmap(brassica_newmap)
+newmapplotflr <- scanone(brassica_newmap, pheno.col = 2, method = "imp", chr = "A10")
+plot(newmapplotflr)
+peak2 <- 9
+newplot_flr <- ggplot(newmapplotflr)
+newplot_flr <- newplot_flr +  theme_bw() + scale_y_continuous(limits=c(0, 13)) + 
+                        geom_line(aes(x = pos, y = lod), size = 2) +
+                        geom_hline(yintercept = 3.08, color = "red", size = 1) +
+                        geom_segment(aes(x = pos, xend = pos), y = (peak * -0.02), yend = (peak * -0.05)) +
+                        theme(text = element_text(size = 20)) +
+                        # theme(legend.position = "none",
+                        #   axis.text.x = element_text(angle = 90),
+                        #   axis.line=element_line())
+                        #   # panel.margin = unit(0, "cm")) +
+                        # ggtitle("LOD Curves for QTLs") +
+                        xlab("Genetic Distance (cM)") +
+                        ylab("LOD Score") 
+newplot_flr
+
+
 new <- scanone(brassica_newmap, pheno.col = 2)
 new2 <- new
 head(new2)
 new2$len <- 1
-new2$xnew   <- 1
+new2$xnew <- 1
 new2$pos
 str(new2)
 new_map_plot <- ggplot(new2, aes(len, pos, ymin = 0, ymax = 40)) + facet_grid(. ~ chr)
